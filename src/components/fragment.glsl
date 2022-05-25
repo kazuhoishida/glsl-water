c00708608c756b2758b2984001277824c12217c5
Kazuho Ishida <k10700110@gmail.com>
2022-05-26 01:05:45 +0900

    add wave effect on mouse pointer over

---
+++ src/components/fragment.glsl
@@ -0,0 +1,11 @@
+export const fragmentShader = `
+precision mediump float;
+
+varying vec2 vUv;
+uniform sampler2D uTexture;
+
+void main() {
+  vec3 texture = texture2D(uTexture, vUv).rgb;
+  gl_FragColor = vec4(texture, 1.0);
+}
+`
