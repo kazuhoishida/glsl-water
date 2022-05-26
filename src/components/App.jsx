ea4d16683c86fd2d2589789526cccd02d0def654
Kazuho Ishida <kazuhoishida@Kazuhos-MacBook-Pro.local>
2022-05-26 11:46:54 +0900

    add bg images & add fade in animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ import { fragmentShader } from "./fragment.glsl"
 import { vertexShader } from "./vertex.glsl"
 import waterImg from "../images/about_picture.jpg"
 import productImg from "../images/firstview_picture_4.jpg"
+import BgImg from "../images/bg-droplet.png"
+import FrontImg from "../images/front-droplet.png"
 import { gsap } from "gsap"
 
 const WaveShaderMaterial = shaderMaterial({ uTexture: new Texture(), repeats: 1, wireframe: false, uTime: 0, noiseAmp: 0.045 }, vertexShader, fragmentShader)
@@ -21,7 +23,7 @@ const Wave = ({ waveImage, freq }) => {
   // onload water animation
   useEffect(() => {
     gsap.to(ref.current, {
-      duration: 4,
+      duration: 2,
       delay: 1,
       noiseAmp: 0.002,
       ease: "power.inOut",
@@ -30,7 +32,7 @@ const Wave = ({ waveImage, freq }) => {
 
   const handlePointerOver = () => {
     gsap.to(ref.current, {
-      duration: 1.5,
+      duration: 1,
       noiseAmp: 0.03,
       ease: "power.inOut",
     })
@@ -64,13 +66,35 @@ const Scene = ({ thumbnail, noiseFreq }) => {
 
 export default function App() {
   const imagesArr = [waterImg, productImg]
+  const canvasRef = useRef()
+
+  // fade in
+  useEffect(() => {
+    console.log(canvasRef.current)
+    gsap.to(canvasRef.current, {
+      opacity: 1,
+      y: 0,
+      duration: 2.4,
+      delay: 0.5,
+      ease: "power.inOut",
+    })
+  }, [])
+
   return (
-    <section className="w-[90vw] mx-auto mt-[10vh]">
-      {imagesArr.map((image) => (
-        <div className="mb-10 h-[30vh]">
-          <Scene thumbnail={image} noiseFreq={1.2} />
+    <div className="relative pt-[10vh] overflow-hidden">
+      <div className="pointer-events-none">
+        <div className="w-full object-cover bg-[#f7f7f7]">
+          <img src={BgImg} alt="" className="absolute top-0 left-0 z-0" />
+          <img src={FrontImg} alt="" className="absolute top-0 left-0 z-50" />
         </div>
-      ))}
-    </section>
+      </div>
+      <section className="w-[90vw] mx-auto z-10 opacity-0 translate-y-10" ref={canvasRef}>
+        {imagesArr.map((image) => (
+          <div className="mb-14 h-[30vh]" key={image}>
+            <Scene thumbnail={image} noiseFreq={1.2} />
+          </div>
+        ))}
+      </section>
+    </div>
   )
 }
